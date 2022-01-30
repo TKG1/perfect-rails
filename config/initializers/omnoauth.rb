b696e844f4f8aae0ad377c919fcce681167e03e6
TKG1 <92356945+TKG1@users.noreply.github.com>
2022-01-31 00:59:42 +0900

    Update omnoauth.rb

--- config/initializers/omnoauth.rb
+++ config/initializers/omnoauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development? || Rails.env.test?
-    provider :github, "60a10a72f7ef7dc8aec7", "29c16a4eeef2b865b13bb86f4ed50b0860d4b9e2"
+    provider :github, "", ""
   else
     provider :github,
     Rails.application.credentials.github[:client_id],
